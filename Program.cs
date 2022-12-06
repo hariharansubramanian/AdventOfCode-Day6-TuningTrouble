7891b9fe61cf5af3f83abc99a83941d5ac75c2f0
Hariharan Subramanian <hari@flowinsights.com>
2022-12-06 22:17:46 +0100

    Using LINQ to find the first unique packet occurance

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Scanning signal...");
+
+var signal = File.ReadAllText("signal_input.txt");
+
+// Puzzle 1
+for (var i = 4; i < signal.Length; i++)
+{
+    var packet = signal.Skip(i - 4).Take(4).ToList();
+    if (packet.Distinct().Count() == 4)
+    {
+        Console.WriteLine($"Found signal at position {i}");
+        break;
+    }
+}
+
+Console.WriteLine("Finished scanning signal.");
