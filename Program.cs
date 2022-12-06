d846f646c106f345be6056071f444b40f494dbf9
Hariharan Subramanian <hari@flowinsights.com>
2022-12-06 22:24:14 +0100

    Using LINQ to find message marker

--- Program.cs
+++ Program.cs
@@ -8,11 +8,20 @@ for (var i = 4; i < signal.Length; i++)
     var packet = signal.Skip(i - 4).Take(4).ToList();
     if (packet.Distinct().Count() == 4)
     {
-        Console.WriteLine($"Found signal at position {i}");
+        Console.WriteLine($"Found start of packet marker at position {i}");
+        break;
+    }
+}
+
+// Puzzle 2
+for (var i = 14; i < signal.Length; i++)
+{
+    var message = signal.Skip(i - 14).Take(14).ToList();
+    if (message.Distinct().Count() == 14)
+    {
+        Console.WriteLine($"Found start of message marker at position {i}");
         break;
     }
 }
 
 Console.WriteLine("Finished scanning signal.");
-
-// Puzzle 2
